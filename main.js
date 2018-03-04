d1847ef35ceb50b3bc5ca9ab1900e424ac9f5c47
Martin Weber <weber_martin@gmx.ch>
2018-03-04 15:25:48 +0100

    npm task for electron hot reload

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
-let win
+// electron live reload
+require('electron-reload')(__dirname);
+
+let mainWindow;
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // load the dist folder from Angular
-  win.loadURL(url.format({
+  mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'dist/index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Open the DevTools optionally:
   // win.webContents.openDevTools()
 
-  win.on('closed', () => {
-    win = null
-  })
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
-
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
+  if (mainWindow === null) {
+    createWindow();
   }
-})
+});
