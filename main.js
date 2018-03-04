3b86e6c722efa594dbb82e32c9d7154d985fd94d
Martin Weber <weber_martin@gmx.ch>
2018-03-04 18:11:47 +0100

    custom menu bar for electron

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const {app, BrowserWindow} = require('electron');
+const {
+  app,
+  BrowserWindow
+} = require('electron');
 const path = require('path');
 const url = require('url');
 
@@ -7,8 +10,12 @@ require('electron-reload')(__dirname);
 
 let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false
+  })
 
   // load the dist folder from Angular
   mainWindow.loadURL(url.format({
